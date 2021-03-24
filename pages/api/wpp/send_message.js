95751936020970ab0ab0e2698f183d71ecd443b5
Vinicius Bassalobre <=>
2021-03-24 01:44:32 -0300

    teste2

--- pages/api/wpp/send_message.js
+++ pages/api/wpp/send_message.js
@@ -5,8 +5,8 @@ const { MessageMedia } = require('whatsapp_engine_js/src/structures')
 
 export default (req, res) => {
 	if (req.method != 'POST') return res.status(405).send("Method Not Allowed")
-	// verifyJWT(req, res)
-	// let { messages, webhook, session } = req.body
+	verifyJWT(req, res)
+	let { messages, webhook, session } = req.body
 
 	// const wpp = new WppClient({ puppeteer: { headless: true }, session })
 
